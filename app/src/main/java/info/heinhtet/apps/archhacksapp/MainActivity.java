1d9b0ce37a6a5b2f3acee55e1eb5b78c5de9fbdb
Hein Htet Zaw <thanlone1995@gmail.com>
2016-11-05 15:28:17 -0500

    Form done and ready to combine with other parts

--- app/src/main/java/info/heinhtet/apps/archhacksapp/MainActivity.java
+++ app/src/main/java/info/heinhtet/apps/archhacksapp/MainActivity.java
@@ -2,24 +2,76 @@ package info.heinhtet.apps.archhacksapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.util.*;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    public String name;
-    public int age;
+    public String user_name, user_age, user_height, user_weight,
+            user_bloodtype, user_organdoner, user_diseases, user_allergies, user_notes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText user_name = (EditText) findViewById(R.id.name);
-        Button submit = (Button) findViewById(R.id.submit);
-        submit.hasOnClickListeners(
-        );
+        final EditText input_name = (EditText) findViewById(R.id.name);
+        final DatePicker input_birthday = (DatePicker) findViewById(R.id.birthday);
+        final EditText input_height_ft = (EditText) findViewById(R.id.height_ft);
+        final EditText input_height_in = (EditText) findViewById(R.id.height_in);
+        final EditText input_weight = (EditText) findViewById(R.id.weight);
+        final RadioGroup input_blood = (RadioGroup) findViewById(R.id.blood);
+        final Switch input_donor = (Switch) findViewById(R.id.donor);
+        final EditText input_diseases = (EditText) findViewById(R.id.diseases);
+        final EditText input_allergies = (EditText) findViewById(R.id.allergies);
+        final EditText input_notes = (EditText) findViewById(R.id.notes);
 
-        name = user_name.getText().toString();
+        final Button submit = (Button) findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v){
+
+                user_name = input_name.getText().toString();
+                user_age = Integer.toString(Calendar.getInstance().get(Calendar.YEAR) - input_birthday.getYear());
+                user_height = input_height_ft.getText().toString() + " ft. " + input_height_in.getText().toString() + " in.";
+                user_weight = input_weight.getText().toString() + " lbs";
+
+                int radioButtonID = input_blood.getCheckedRadioButtonId();
+                View radioButton = input_blood.findViewById(radioButtonID);
+                int id = input_blood.indexOfChild(radioButton);
+                RadioButton button = (RadioButton) input_blood.getChildAt(id);
+                user_bloodtype = button.getText().toString();
+
+                if (input_donor.isChecked()) user_organdoner = "Organ Donor"; else user_organdoner = "Not Organ Donor";
+                user_diseases = "[ " + input_diseases.getText().toString() + " ]";
+                user_allergies = "[ " +input_allergies.getText().toString()+ " ]";
+                user_notes = "[" +input_notes.getText().toString()+ " ]";
+
+                System.out.println(user_name);
+                System.out.println(user_age);
+                System.out.println(user_height);
+                System.out.println(user_weight);
+                System.out.println(user_bloodtype);
+                System.out.println(user_organdoner);
+                System.out.println(user_diseases);
+                System.out.println(user_allergies);
+                System.out.println(user_notes);
+
+            }
+
+        });
+
     }
 }
